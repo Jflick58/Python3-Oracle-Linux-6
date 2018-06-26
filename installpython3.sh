12f136e4e281f432db64ed1391788367fd8276ff
Justin Flick <Justinjayflick@gmail.com>
2018-06-25 18:42:35 -0700

    Create installpython3.sh

---
+++ installpython3.sh
@@ -0,0 +1,17 @@
+--
+-- Installing python 3.6.5 on OEL 6.1 from source
+--
+sudo yum install tk-devel gdbm-devel
+mkdir python
+cd python
+export http_proxy=acme.proxy.com:80
+wget http://www.python.org/ftp/python/3.6.5/Python-3.6.5.tgz
+tar xvzf Python-3.6.5.tgz
+cd Python-3.6.5
+sudo make install
+sudo ln -s /opt/python3.6/bin/python3.6 /usr/bin/python36
+sudo ln -s /opt/python3.6/bin/idle3.6 /usr/bin/idle-python36
+sudo ln -s /opt/python3.6/bin/pip3 /usr/bin/pip
+ 
+-- run test:
+python36 
