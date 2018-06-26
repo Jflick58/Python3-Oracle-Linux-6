845a76c504dc0ed323c687aaecb2f32d55b5742d
Justin Flick <Justinjayflick@gmail.com>
2018-06-26 12:11:28 -0700

    Update installpython3.sh

--- installpython3.sh
+++ installpython3.sh
@@ -7,7 +7,11 @@ cd python
 export http_proxy=acme.proxy.com:80
 wget http://www.python.org/ftp/python/3.6.5/Python-3.6.5.tgz
 tar xvzf Python-3.6.5.tgz
+echo $PWD
 cd Python-3.6.5
+./configure --prefix=/opt/python3.6
+echo $PWD
+make
 sudo make install
 sudo ln -s /opt/python3.6/bin/python3.6 /usr/bin/python36
 sudo ln -s /opt/python3.6/bin/idle3.6 /usr/bin/idle-python36
